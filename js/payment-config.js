9f7cfa503c72fd57b413fbbd1b7d72e351058fb2
Abraham Gift <abrahamgift788@gmail.com>
2026-09-04 09:35:03 +0100

    Expose Paystack checkout endpoint configuration

--- js/payment-config.js
+++ js/payment-config.js
@@ -4,5 +4,6 @@ window.VISUAL_TECH_PAYMENT = {
     provider: "paystack",
     environment: "test",
     publicKey: "pk_test_ae6f2789406e7e7b807f712a161902e525c76307",
-    currency: "NGN"
+    currency: "NGN",
+    initializeFunction: "paystack-initialize-transaction"
 };
